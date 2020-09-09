d4958025b20d446255f2310ed2441adc1cc9af83
andres5428 <andres54jaramillo@gmail.com>
2020-09-09 14:34:31 -0500

    middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,36 +1,35 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const router = express.Router();
-const {URI,DB_DATABASE} = require('../config');
+const { URI, DB_DATABASE } = require('../config');
 
 const clientPromise = mongodb.MongoClient.connect(URI); // Retorna una promesa
 
-let data_try= [];
-
-const validate = ((req,res,next)=>{
-  
-    if (data_try.length === 0){
-      res.status(400).end("el valor no se encuentra")
-      next(Error);
-    }
-    else {
-      res.status(200).end(req.body.name);
-      next();
-    }
-    res.status(500).json({message: error.message});
-})
+async function validate(req, res, next) {
+  const client = await clientPromise;
+  const database = client.db(DB_DATABASE);
+  const collection = database.collection('comidas');
+  const data = await collection.find({ "name": req.params.name }).toArray()
+  if (data.length === 0) {
+    return res.status(404).json({ message: 'El plato no existe' })
+  }
+  else {
+    // res.status(200).json({message: 'El plato si existe'})
+    next()
+  }
+}
 
 
 /**
  *  GET - Retorna todos los platos
  */
-router.get('/food', async (req,res)=>{
-    const client = await clientPromise;
-    const database = client.db(DB_DATABASE);
-    const collection = database.collection('comidas');
-    console.log(collection)
-    const data = await collection.find({}).toArray()
-    res.json({data});
+router.get('/food', async (req, res) => {
+  const client = await clientPromise;
+  const database = client.db(DB_DATABASE);
+  const collection = database.collection('comidas');
+  console.log(collection)
+  const data = await collection.find({}).toArray()
+  res.json({ data });
 });
 
 
@@ -38,66 +37,64 @@ router.get('/food', async (req,res)=>{
 /**
  * GET - Buscar plato en específico
  */
-router.get('/food/:name', async (req,res)=>{
+router.get('/food/:name', validate, async (req, res) => {
   const client = await clientPromise;
   const database = client.db(DB_DATABASE);
   const collection = database.collection('comidas');
-  console.log(collection)
-  const data = await collection.find({"name":req.params.name}).toArray()
-  console.log(data)
+  const data = await collection.find({ "name": req.params.name }).toArray()
   data_try = data;
-  res.json({data});
+  res.json({ data });
 });
 
 /**
  * Post - Ingresar un plato de comida
  */
-router.post('/food', async (req,res)=>{
-    const client = await clientPromise;
-    const database = client.db(DB_DATABASE);
-    const collection = database.collection('comidas');
-    console.log(req.body)
-    console.log(req.body.origin)
-    try {
-      const data = await collection.save({"name":req.body.name,"origin":req.body.origin});
-      res.status(202).json(data);
-    } catch (error) {
-      res.status(500).json({message: error.message});
-    }
+router.post('/food', async (req, res) => {
+  const client = await clientPromise;
+  const database = client.db(DB_DATABASE);
+  const collection = database.collection('comidas');
+  console.log(req.body)
+  console.log(req.body.origin)
+  try {
+    const data = await collection.save({ "name": req.body.name, "origin": req.body.origin });
+    res.status(202).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 /**
  * Put - Modificar un plato de comida
  */
-router.put('/food/', async (req,res)=>{
-    const client = await clientPromise;
-    const database = client.db(DB_DATABASE);
-    const collection = database.collection('comidas');
-    try {
-      const data = await collection.update({"name":req.body.name},{"name":req.body.modifyname})
-      res.status(202).json(data); // arreglar el $set
-    } catch (error) {
-      res.status(500).json({message: error.message});
-    }
+router.put('/food/', async (req, res) => {
+  const client = await clientPromise;
+  const database = client.db(DB_DATABASE);
+  const collection = database.collection('comidas');
+  try {
+    const data = await collection.update({ "name": req.body.name }, { "name": req.body.modifyname })
+    res.status(202).json(data); // arreglar el $set
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 /**
  * Delete - Modificar un plato de comida
  */
-router.delete('/food/', async (req,res)=>{
-    const client = await clientPromise;
-    const database = client.db(DB_DATABASE);
-    const collection = database.collection('comidas');
-    try {
-      const data = await collection.remove({"name":req.body.name})
-      res.status(202).json(data);
-    } catch (error) {
-      res.status(500).json({message: error.message});
-    }
+router.delete('/food/', async (req, res) => {
+  const client = await clientPromise;
+  const database = client.db(DB_DATABASE);
+  const collection = database.collection('comidas');
+  try {
+    const data = await collection.remove({ "name": req.body.name })
+    res.status(202).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 
 
 module.exports = {
-    users: router
+  users: router
 }
